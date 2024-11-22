c441558c14274ef00dc06ff0b31532aaf63f059c
endijulian <endijulian080798@gmail.com>
2024-11-22 10:19:55 +0700

    if else and while loops

---
+++ if_else.py
@@ -0,0 +1,71 @@
+# a = 33
+# b = 200
+# if b > a:
+#     print("b is greater than a")
+
+# a = 33
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+
+# a = 200
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+# else:
+#     print("a is greater than b")
+
+# a = 200
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is greater than a")
+
+
+# Short Hand If
+# if a > b: print("a is greater than b")
+
+# a = 2 
+# b = 33
+# print("A") if a > b else print("B")
+
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a == b else print("B")
+
+# a = 200
+# b = 33
+# c = 500
+
+# if a > b and c > a:
+#     print("Both conditionals are True")
+
+# if a > b or a > c:
+#     print("At least one of the conditionals is True")  # Output: At least one
+
+# a = 33
+# b = 200
+# if not a > b:
+#     print("a is NOT greater than b")
+
+
+# Nested If
+# x = 41
+
+# if x > 10:
+#     print("Above ten,")
+#     if x > 20:
+#         print("and also above 20")
+#     else:
+#         print("but not over 20")
+
+a = 33
+b = 200
+
+if a > b:
+    pass
