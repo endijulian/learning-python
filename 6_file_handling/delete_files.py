904f2ecc1cf6f1e3fc3ca0e0445ec1911dec5754
endijulian <endijulian080798@gmail.com>
2024-11-25 11:46:57 +0700

    file handling

---
+++ 6_file_handling/delete_files.py
@@ -0,0 +1,10 @@
+import os
+
+# os.remove("myfile.txt")
+
+# if os.path.exists("demofile.txt"):
+#     os.remove("demofile.txt")
+# else:
+#     print("The file does not exist")
+
+os.rmdir("test")
---
+++ 6_file_handling/handling_file.py
@@ -0,0 +1,15 @@
+f = open("demofile.txt", "r")
+# f = open("D:\\myfiles\welcome.txt", "r") ##Buka file di lokasi berbeda:
+# print(f.read())
+# print(f.read(5))
+# print(f.readline())
+# print(f.readline())
+
+
+# for x in f:
+#     print(x)
+
+
+# Close Files
+print(f.readline())
+f.close()
